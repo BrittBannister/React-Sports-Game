48f9491bda7ab8dbe3dc438e271dace91bf05618
Brittney Bannister <bbannister1127@icloud.com>
2020-08-11 18:11:55 -0400

    daveys code at CRA

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Game from './Components/Game/Game'
+
+function App(props) {
+    const raccoons = {
+      name: 'Russiaville Raccoons',
+      logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/raccoon.png'
+    }
+  
+    const squirrels = {
+      name: 'Sheridan Squirrels',
+      logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/squirrel.png'
+    }
+  
+    const bunnies = {
+      name: 'Burlington Bunnies',
+      logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/bunny.png'
+    }
+  
+    const hounds = {
+      name: 'Hammond Hounds',
+      logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/hound.png'
+    }
+  
+    return (
+      <div className="App">
+        <Game
+          venue="Union 525 Gem"
+          homeTeam={squirrels}
+          visitingTeam={raccoons}
+        />
+        <Game
+          venue="Sheridan Arena"
+          homeTeam={bunnies}
+          visitingTeam={hounds}
+        />
+      </div>
+    )
+  }
+
+
+
+
+export default App
